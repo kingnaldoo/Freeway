415f90c96dc71cb86d82f51731c4b01b7ce6395c
kingnaldoo <reinaldosn2016@gmail.com>
2023-07-05 10:46:18 -0300

    fix: uuid

--- Score.js
+++ Score.js
@@ -1,5 +1,6 @@
 class Score {
     constructor(id, score = 0, x = 10, y = 30) {
+        this.id = id;
         this.score = score;
         this.x = x;
         this.y = y;
@@ -23,7 +24,7 @@ class Score {
         if (playerPosY <= 25) {
             this.score++;
 
-            setFirestore("users", World.canvas.id, {
+            setFirestore("users", this.id, {
                 score: this.score
             });
         }
